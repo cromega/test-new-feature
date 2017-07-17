e165ab2676eff01f2ac7284b254c8d23cb2f75dd
Bence Monus <crome@moronia.hu>
2017-07-18 00:48:12 +0100

    add test for multiple addresses

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,8 @@ describe User do
 
   it "can have multiple addresses" do
     user = User.new(username: "asd")
-    fail "test needs implementation"
+    user.addresses.create(address: "some street", postcode: "n78sddsdfdf")
+    user.addresses.create(address: "some other street", postcode: "n78asd")
+    expect(user.addresses.count).to eq 2
   end
 end
